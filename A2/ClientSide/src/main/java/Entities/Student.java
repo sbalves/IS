828a0b1244794d4cb10e93b0857d1f809b12b804
João Dionísio <73122818+JoaopDionisio@users.noreply.github.com>
2022-10-31 15:36:01 +0000

    Add files via upload

--- A2/ClientSide/src/main/java/Entities/Student.java
+++ A2/ClientSide/src/main/java/Entities/Student.java
@@ -7,8 +7,41 @@ public class Student {
     private Integer completed__credits;
     private Integer average_grade;
 
-    //Over-riding the toString() function as a class function
-    public String toString(){
-        return "name: " + this.name;
+
+    public String getName() {
+        return name;
     }
+
+    public String getBirth_date() {
+        return birth_date;
+    }
+
+    public Integer getCompleted__credits() {
+        return completed__credits;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirth_date(String birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public void setCompleted__credits(Integer completed__credits) {
+        this.completed__credits = completed__credits;
+    }
+
+    public void setAverage_grade(Integer average_grade) {
+        this.average_grade = average_grade;
+    }
+
+    public Integer getAverage_grade() {
+        return average_grade;
+    }
+
+    public String toString(){
+        return "name: " + this.name + "birth date: " + this.birth_date + "completed credits: " + this.completed__credits + "average grade: " + this.average_grade;
+    }
+
 }
--- A2/ClientSide/src/main/java/com/example/ClientSide/ClientSideApplication.java
+++ A2/ClientSide/src/main/java/com/example/ClientSide/ClientSideApplication.java
@@ -3,9 +3,10 @@ package com.example.ClientSide;
 import Entities.Student;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class ClientSideApplication {
-
+	/*
 	public static Flux<Student> getstudents() {
 		WebClient client = WebClient.create();
 		return client.get()
@@ -13,25 +14,35 @@ public class ClientSideApplication {
 				.retrieve()
 				.bodyToFlux(Student.class);
 	}
+	*/
 
-	/*
-	public static void getstudents(){
-		WebClient client = WebClient.create();
-		WebClient.ResponseSpec responseSpec = client.get()
-				.uri("http://localhost:8080/api/student")
-				.retrieve()
-				.bodyToMono(Student.class);;
+	//2. Number of students
+	public static void getNumberStudents(Flux<Student> fluxStudents){
+		System.out.println(fluxStudents.count().block());
+	}
 
+	public static void getStudentsBirthDate(Flux<Student> fluxStudents){
 
-		String responseBody = responseSpec.bodyToMono(String.class).block();
-		System.out.println(responseBody);
+		fluxStudents.subscribe(System.out::println);
+		//studentsFlux.subscribe(c-> System.out.println(c.getBirth_date()));
+		//System.out.println(responseSpec);
 
 	}
-	*/
+
 	public static void main(String[] args) {
+		/*
 		Flux<Student> students = getstudents();
 		students.subscribe(System.out::println);
+		students.doOnNext(c->System.out.println(c.getBirth_date()));
+		*/
+		WebClient client = WebClient.create();
+		Flux<Student> fluxStudents = client.get()
+				.uri("http://localhost:8080/api/student")
+				.retrieve()
+				.bodyToFlux(Student.class);
 
 
+		getNumberStudents(fluxStudents);
+		getStudentsBirthDate(fluxStudents);
 	}
 }
